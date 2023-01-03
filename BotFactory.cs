0b10a656acec07a741b5bc9244e419f92e118cce
Benjamin Meshanko <70403274+Bmeshanko@users.noreply.github.com>
2023-01-03 15:24:16 -0500

    Add files via upload

---
+++ BotFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarLight.Shared.AI
+{
+    public static class BotFactory
+    {
+        public static readonly string[] Names = new[] { "Wunderwaffe", "Prod", "ProdRandom", "Cowzow", "Prime" };
+
+        public static IWarLightAI Construct(string name)
+        {
+            switch (name.ToLower())
+            {
+                case "wunderwaffe":
+                    return new Wunderwaffe.Bot.BotMain();
+                case "prod":
+                    return new Prod.BotMain(false);
+                case "prodrandom":
+                    return new Prod.BotMain(true);
+                case "cowzow":
+                    return new Cowzow.Bot.CowzowBot();
+                case "prime":
+                    return new Prime.Main.PrimeBot();
+                default:
+                    throw new Exception("No bot found named " + name + ", supported names are: " + Names.JoinStrings(", "));
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Warzone-Bot
+Making a bot to play Warzone on https://www.warzone.com/.
